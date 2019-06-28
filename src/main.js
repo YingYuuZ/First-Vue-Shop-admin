194fc9f01a93f7884d6e502c0c52ffda1891c44b
zhangyingyu <1047714069@qq.com>
2019-06-28 16:25:07 +0800

    Login基本结构完成

--- src/main.js
+++ src/main.js
@@ -1,6 +1,14 @@
 import Vue from 'vue'
+
+// elementUI
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
+
 import App from './App.vue'
 import router from './router'
+
+import './assets/css/index.css'
+Vue.use(ElementUI)
 
 Vue.config.productionTip = false
 
